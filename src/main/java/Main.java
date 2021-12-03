ce9811635be4f97dd929883fd36fda9fc7fc7b4e
Admin <pidus@o2.pl>
2021-12-03 19:27:53 +0100

    Maven lesson 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.google.gson.Gson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book book = new Book("Tolkien", "Lord of the rings");
+
+        Gson gson = new Gson();
+        String json = gson.toJson(book);
+
+        System.out.println(json);
+    }
+}

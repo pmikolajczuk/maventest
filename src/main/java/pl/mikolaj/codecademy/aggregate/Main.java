d6d585002e603c8784cccaac9ee8bd9924a67ede
pmikolajczuk <pidus@o2.pl>
2023-02-11 14:28:45 +0100

    Codecademy Aggregate - doubleEvenList, specialWords v1

---
+++ src/main/java/pl/mikolaj/codecademy/aggregate/Main.java
@@ -0,0 +1,64 @@
+package pl.mikolaj.codecademy.aggregate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = List.of(5, 4, 1, 3, 7, 8);
+
+        for (Integer x : doubleEvenList(intList)) {
+            System.out.print(x + ", ");
+        }
+        System.out.println();
+
+        List<String> words = new ArrayList<>();
+        words.add("Tree");
+        words.add("Hello");
+        words.add("World");
+        words.add("Race");
+        words.add("Game");
+        words.add("Numbers");
+        words.add("Ride");
+
+        List<String> specialWordsWithForLoop = getSpecialWordsWithForLoop(words);
+        List<String> specialWordsWithPipeline = getSpecialWordsWithPipeline(words);
+        System.out.println("Special Words retrieved from for loop: " + specialWordsWithForLoop);
+        System.out.println("Special Words retrieved from pipeline: " + specialWordsWithPipeline);
+    }
+
+    private static List<String> getSpecialWordsWithForLoop(List<String> words) {
+        List<String> result = new ArrayList<>();
+        for (String word : words) {
+            if (word.length() == 4) {
+                result.add(word.toUpperCase());
+            }
+        }
+        return result;
+    }
+
+    private static List<String> getSpecialWordsWithPipeline(List<String> words) {
+        return words.stream()
+                .filter(s -> s.length() == 4)
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> doubleEvenList(List<Integer> intList) {
+        return intList.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 2)
+                .toList();
+    }
+
+    public static List<Integer> doubleEvenList2(List<Integer> intList) {
+        List<Integer> evenList = new ArrayList<>();
+        for (Integer x : intList) {
+            if (x % 2 == 0) {
+                evenList.add(x * 2);
+            }
+        }
+        return evenList;
+    }
+}

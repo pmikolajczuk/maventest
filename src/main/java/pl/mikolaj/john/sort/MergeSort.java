e0e72f39d733538fe50f29ab47474957be2a992c
pmikolajczuk <pidus@o2.pl>
2023-03-01 18:51:16 +0100

    Merge Sort V2

--- src/main/java/pl/mikolaj/john/sort/MergeSort.java
+++ src/main/java/pl/mikolaj/john/sort/MergeSort.java
@@ -4,18 +4,17 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int[] numbers = Utils.generateArray(100_000);
+        int[] numbers = Utils.generateArray(10);
         //int[] numbers = {3, 1};//, 8, 6, 2};
         System.out.println("Before: ");
         Utils.printArray(numbers);
 
-        mergeSort(numbers);
+        mergeSortV2(numbers);
         System.out.println("After:");
         Utils.printArray(numbers);
     }
 
     private static void mergeSort(int[] numbers) {
-        //Utils.printArray(numbers);
         if (numbers.length == 1) {
             return;
         }
@@ -39,11 +38,11 @@ public class MergeSort {
             }
         }
 
-        for(; i < left.length; i++, k++) {
+        for (; i < left.length; i++, k++) {
             result[k] = left[i];
         }
 
-        for(; j < right.length; j++, k++) {
+        for (; j < right.length; j++, k++) {
             result[k] = right[j];
         }
     }
@@ -53,12 +52,47 @@ public class MergeSort {
     }
 
     private static void sort(int[] numbers, int first, int last) {
-        Utils.printArray(numbers, first, last);
         if (first == last) {
             return;
         }
         int middle = (first + last) / 2;
         sort(numbers, first, middle); //sort left
         sort(numbers, middle + 1, last); //sort right
+        merge(numbers, first, middle, last);
+    }
+
+    private static void merge(int[] numbers, int first, int middle, int last) {
+        int[] left = copyOfRange(numbers, first, middle + 1);
+        int[] right = copyOfRange(numbers, middle + 1, last + 1);
+
+        int i = 0, j = 0, k = first;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                numbers[k] = left[i];
+                i++;
+            } else {
+                numbers[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < left.length) {
+            numbers[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < right.length) {
+            numbers[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    private static int[] copyOfRange(int[] numbers, int first, int last) {
+        int[] result = new int[last - first];
+        for (int i = first; i < last; i++) {
+            result[i - first] = numbers[i];
+        }
+        return result;
     }
 }

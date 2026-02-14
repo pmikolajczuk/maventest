8b67f7e52cdc94332c284044d68d707d9c9860c8
pmikolajczuk <pidus@o2.pl>
2026-02-14 18:57:23 +0100

    2026 February FizzBuzz

--- src/main/java/pl/mikolaj/r2026/utils/Utils.java
+++ src/main/java/pl/mikolaj/r2026/utils/Utils.java
@@ -1,5 +1,10 @@
 package pl.mikolaj.r2026.utils;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Utils {
@@ -17,5 +22,19 @@ public class Utils {
 			}
 		}
 		return sb.toString();
+	}
+
+	public static <K, V> void printMultiValueMap(Map<K, List<V>> result) {
+		result.forEach(Utils::printMultiValueMapEntry);
+	}
+
+	private static <K, V> void printMultiValueMapEntry(K k, List<V> vs) {
+		System.out.print(k + ": ");
+		String integersString = Optional.ofNullable(vs)
+				.orElse(Collections.emptyList())
+				.stream()
+				.map(String::valueOf)
+				.collect(Collectors.joining(","));
+		System.out.println(integersString);
 	}
 }
